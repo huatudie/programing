d39d25c83e184d718f5932749a27771d30f1fdb5
usera001 <2734276426@qq.com>
2022-09-07 10:48:36 +0800

    caculate.cs is created .

---
+++ caculate.cs
@@ -0,0 +1,15 @@
+$ cat caculate.cs
+public static class caculate
+{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int max(int a,int b)
+	{
+		if(a>b)
+			return a;
+		else
+			return b;
+	}
+}
